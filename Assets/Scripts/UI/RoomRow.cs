7faabc6d99ad0fa50e8db5135d32739aa2788114
Lithrun <nickwindt@hotmail.nl>
2020-03-25 21:07:43 +1300

    Added Title Screen & Basic Lobby Overview

---
+++ Assets/Scripts/UI/RoomRow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomRow : MonoBehaviour
+{
+    public string Room;
+    public bool IsJoinable = true;
+    // Use this for initialization
+	void Start () 
+    {
+        GetComponentInChildren<Text>().text = Room;
+    }
+
+    public void JoinLobby()
+    {
+        if (!IsJoinable) return;
+        PhotonNetwork.JoinRoom(Room);
+    }
+}
