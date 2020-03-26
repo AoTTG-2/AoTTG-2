0e59b56dcdf7b2d9709ebc7679d621e0aaf31c78
Lithrun <nickwindt@hotmail.nl>
2020-03-26 17:48:05 +1300

    #18 Sync to Development (#51)
    * Added Title Screen & Basic Lobby Overview

--- Assets/Scripts/UI/ServerRegion.cs
+++ Assets/Scripts/UI/ServerRegion.cs
@@ -1,21 +1,58 @@
-﻿using System;
-using System.Diagnostics;
+﻿using UnityEngine;
 
 namespace Assets.Scripts.UI
 {
     public class ServerRegion : UiElement
     {
-        public void JoinRegion(string region)
+        public GameObject ScrollViewContent;
+        public GameObject Row;
+        private int Region { get; set; }
+
+        public void OnEnable()
         {
-            var cloudRegion = (CloudRegionCode) Enum.Parse(typeof(CloudRegionCode), region.ToLower());
-            if (PhotonNetwork.ConnectToRegion(cloudRegion, "2020"))
+            PhotonNetwork.ConnectToRegion((CloudRegionCode)Region, "2020");
+        }
+
+        public void OnRegionChanged(int region)
+        {
+            Region = region;
+            PhotonNetwork.Disconnect();
+
+        }
+
+        public void OnDisconnectedFromPhoton()
+        {
+            PhotonNetwork.ConnectToRegion((CloudRegionCode) Region, "2020");
+        }
+
+        public void OnConnectedToPhoton()
+        {
+            CancelInvoke("RefreshLobby");
+            InvokeRepeating("RefreshLobby", 2f, 5f);
+        }
+
+        void RefreshLobby()
+        {
+            foreach (Transform child in ScrollViewContent.transform)
             {
-                // Succeeded    
-                Navigate(typeof(ServerList));
+                Destroy(child.gameObject);
             }
-            else
+
+            var rooms = PhotonNetwork.GetRoomList();
+
+            if (rooms.Length == 0)
             {
-                // Rip
+                var row = Instantiate(Row, ScrollViewContent.transform);
+                var room = row.GetComponent<RoomRow>();
+                room.Room = "No Lobbies available...";
+                room.IsJoinable = false;
+                return;
+            }
+
+            foreach (var roomInfo in rooms)
+            {
+                var row = Instantiate(Row, ScrollViewContent.transform);
+                row.GetComponent<RoomRow>().Room = roomInfo.Name;
             }
         }
     }
