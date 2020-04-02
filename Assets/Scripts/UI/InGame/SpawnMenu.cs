e9e7a29f0c49ea54f12983f18956ae0c2cc57813
Lithrun <nickwindt@hotmail.nl>
2020-04-02 18:03:44 +1300

    Improved spawn button, and added Titan Health

--- Assets/Scripts/UI/InGame/SpawnMenu.cs
+++ Assets/Scripts/UI/InGame/SpawnMenu.cs
@@ -1,13 +1,79 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.UI.InGame
 {
     public class SpawnMenu : MonoBehaviour
     {
+        public Dropdown EquipmentDropdown;
+
+        private void Start()
+        {
+            EquipmentDropdown.options = new List<Dropdown.OptionData>();
+            foreach (var equipment in Enum.GetNames(typeof(EquipmentType)))
+            {
+                EquipmentDropdown.options.Add(new Dropdown.OptionData(equipment));
+            }
+            EquipmentDropdown.captionText.text = EquipmentDropdown.options[0].text;
+        }
+
         public void Spawn()
         {
-            AottgUi.TestSpawn();
+            string selection = "23";
+            GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().needChooseSide = false;
+            if (!PhotonNetwork.isMasterClient && (GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().roundTime > 60f))
+            {
+                if (!isPlayerAllDead2())
+                {
+                    GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().NOTSpawnPlayer(selection);
+                }
+                else
+                {
+                    GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().NOTSpawnPlayer(selection);
+                    GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().photonView.RPC("restartGameByClient", PhotonTargets.MasterClient, new object[0]);
+                }
+            }
+            else if (((FengGameManagerMKII.Gamemode.GamemodeType == GamemodeType.TitanRush) || (FengGameManagerMKII.Gamemode.GamemodeType == GamemodeType.Trost)) || FengGameManagerMKII.Gamemode.GamemodeType == GamemodeType.Capture)
+            {
+                GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().SpawnPlayer(selection, "playerRespawn");
+                if (isPlayerAllDead2())
+                {
+                    GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().NOTSpawnPlayer(selection);
+                    GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().photonView.RPC("restartGameByClient", PhotonTargets.MasterClient, new object[0]);
+                }
+            }
+            else
+            {
+                GameObject.Find("MultiplayerManager").GetComponent<FengGameManagerMKII>().SpawnPlayer(selection, "playerRespawn");
+            }
+            IN_GAME_MAIN_CAMERA.usingTitan = false;
+            GameObject.Find("MainCamera").GetComponent<IN_GAME_MAIN_CAMERA>().setHUDposition();
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(PhotonPlayerProperty.character, selection);
+            Hashtable propertiesToSet = hashtable;
+            PhotonNetwork.player.SetCustomProperties(propertiesToSet);
             gameObject.SetActive(false);
+        }
+
+        private static bool isPlayerAllDead2()
+        {
+            int num = 0;
+            int num2 = 0;
+            foreach (PhotonPlayer player in PhotonNetwork.playerList)
+            {
+                if (RCextensions.returnIntFromObject(player.CustomProperties[PhotonPlayerProperty.isTitan]) == 1)
+                {
+                    num++;
+                    if (RCextensions.returnBoolFromObject(player.CustomProperties[PhotonPlayerProperty.dead]))
+                    {
+                        num2++;
+                    }
+                }
+            }
+            return (num == num2);
         }
 
         public void Update()
