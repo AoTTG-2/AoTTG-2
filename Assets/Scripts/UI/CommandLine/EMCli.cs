cf22b7610664f087abb0865823ad3951527a6c53
TicExp <61810846+TicExp@users.noreply.github.com>
2020-04-07 10:07:31 +0300

    Command line enhancements, Color, position etc

--- Assets/Scripts/UI/CommandLine/EMCli.cs
+++ Assets/Scripts/UI/CommandLine/EMCli.cs
@@ -8,25 +8,35 @@ public class EMCli : Photon.MonoBehaviour
 {
     private static float layoutWidth = 300f;
     private static float floatShift = layoutWidth + 25;
-    private static Rect rectMainGUI = new Rect(Screen.width - 320f, 0, layoutWidth, 430f);
+    private static Rect rectMainGUI = new Rect(0, 0, layoutWidth, 430f);
     private static Rect rectSuggestionLayout = rectMainGUI;
     private static Rect rectSuggestionsLayoutShift = new Rect(320f, 0, layoutWidth, 430f);
     private static GUILayoutOption[] guiLayoutOptionMainGUI = new GUILayoutOption[] { GUILayout.Width(320f), GUILayout.MaxHeight(430f) };
     private static GUI.WindowFunction windowFunctionMainGUI = new GUI.WindowFunction(WindowLayoutMainGUI);
     private static string layout = string.Empty;
     private static Vector2 scrollPosition = Vector2.zero;
-    private static string nameOfControle = "CLI";
-    private static string head = "Command Line".RepaintGreen(true);
-    private static string arrow = ">".RepaintGreen(true);
-    private static string delete = "x".RepaintError(true);
     public bool Visible = true;
     private static bool readyToJoinOrCreateRoom = false;
     private static Vector2 scrollPositionSuggestions = Vector2.zero;
     private static string suggestions = string.Empty;
 
+    private static Color backgroundColor = Color.black; //use whatever color you like
+    private static string textColorCode = "[ffffff]";
+    private static string crossColorCode = "[ff0000]";
+    private static string enterColorCode = "[ffffff]";
+
+    private static string nameOfControle = "CLI";
+    private static string head = "Command Line".RepaintCustom(textColorCode, true);
+    private static string arrow = ">".RepaintCustom(enterColorCode, true);
+    private static string delete = "x".RepaintCustom(crossColorCode, true);
+    private static KeyCode keyCodeShowHide = KeyCode.BackQuote; //Show/Hide console
+    private static KeyCode keyCodeFocus = KeyCode.RightAlt; //Press this and start typing right away
+    private static KeyCode keyCodeEnter = KeyCode.Return; //Enter a command
+
     // Use this for initialization
     void Start()
     {
+        UnityEngine.Object.DontDestroyOnLoad(base.gameObject);
         EnterCommand("/info");
     }
 
@@ -56,7 +66,7 @@ public class EMCli : Photon.MonoBehaviour
     {
         if (Event.current.type == EventType.KeyUp)
         {
-            if (Event.current.keyCode == KeyCode.RightAlt)
+            if (Event.current.keyCode == keyCodeFocus)
             {
                 if (GUI.GetNameOfFocusedControl() == nameOfControle)
                 {
@@ -74,7 +84,7 @@ public class EMCli : Photon.MonoBehaviour
                 {
                     if (Event.current.keyCode == KeyCode.RightControl)
                     {
-                        InputLine.Switch(); 
+                        InputLine.Switch();
                     }
                 }
             }
@@ -87,7 +97,7 @@ public class EMCli : Photon.MonoBehaviour
             {
                 InputLine.Down();
             }
-            else if (Event.current.keyCode == KeyCode.F3)
+            else if (Event.current.keyCode == keyCodeShowHide)
             {
                 Visible = !Visible;
             }
@@ -95,26 +105,26 @@ public class EMCli : Photon.MonoBehaviour
         }
         else if (Event.current.type == EventType.KeyDown)
         {
-            
+
         }
-        
-        if(GUI.GetNameOfFocusedControl() != nameOfControle && InputLine.IsSuggestionModeEnabled)
+
+        if (GUI.GetNameOfFocusedControl() != nameOfControle && InputLine.IsSuggestionModeEnabled)
         {
             InputLine.Switch();
         }
 
-         if (Visible) EMCliGUI();
+        if (Visible) EMCliGUI();
     }
 
     public static void AddLine(string line)
     {
-        layout = string.Concat(layout, '\n', ">".RepaintGreen(true), line);
+        layout = string.Concat(layout, '\n', arrow, line);
         scrollPosition += new Vector2(0, 150);
     }
 
     public void EMCliGUI()
     {
-        GUI.backgroundColor = Color.green;
+        GUI.backgroundColor = backgroundColor;
         rectMainGUI = GUILayout.Window(208, rectMainGUI, windowFunctionMainGUI, head, guiLayoutOptionMainGUI);
         if (!string.IsNullOrEmpty(InputLine.inputLine))
         {
@@ -125,7 +135,7 @@ public class EMCli : Photon.MonoBehaviour
     private static string getRelevantSuggestions()
     {
         string relevantSuggestions = string.Empty;
-        foreach(Command command in CommandHandler.Instance.Commands)
+        foreach (Command command in CommandHandler.Instance.Commands)
         {
             if (command.Format.StartsWith(InputLine.inputLine))
             {
@@ -134,10 +144,10 @@ public class EMCli : Photon.MonoBehaviour
         }
         return relevantSuggestions;
     }
-    
+
     public static void WindowLayoutMainGUI(int windowID)
     {
-        GUI.backgroundColor = Color.green;
+        GUI.backgroundColor = backgroundColor;
         GUILayout.BeginHorizontal();
         if (GUILayout.Button(arrow, GUILayout.MaxWidth(25)))
         {
@@ -167,7 +177,7 @@ public class EMCli : Photon.MonoBehaviour
         {
             if (Event.current.type == EventType.KeyUp)
             {
-                if ((Event.current.keyCode == KeyCode.Return) && GUI.GetNameOfFocusedControl().Equals(nameOfControle))
+                if ((Event.current.keyCode == keyCodeEnter) && GUI.GetNameOfFocusedControl().Equals(nameOfControle))
                 {
                     if (!string.IsNullOrEmpty(InputLine.inputLine))
                     {
@@ -227,10 +237,9 @@ public class EMCli : Photon.MonoBehaviour
         };
         PhotonNetwork.PhotonServerSettings.JoinLobby = true;
         PhotonNetwork.JoinOrCreateRoom("TestServer`The City`abnormal`999999`day``1", roomOptions, TypedLobby.Default);
-        //ServerList.instance.gameObject.SetActive(false);
         "Creating a room...".SendProcessing(true);
     }
-    
+
     private void OnConnectedToMaster()
     {
         "Connected to master".SendWarning(true);
