4f5ba215c0c1d2374e59d544ed2b01c531d9f8c6
ThatProBro2535 <agloria2001@gmail.com>
2020-05-02 18:53:38 -0600

    Resolution changing works

--- Assets/Scripts/UI/InGame/Graphics/ResolutionSwitcher.cs
+++ Assets/Scripts/UI/InGame/Graphics/ResolutionSwitcher.cs
@@ -7,15 +7,30 @@ public class ResolutionSwitcher : MonoBehaviour {
 
 	public Dropdown dropdown;
 	public Toggle toggle;
+	public Text label;
 	private void OnEnable() {
+		
 		foreach(Resolution resolution in Screen.resolutions)
 		{
 			string resolutionText = resolution.ToString().Split('@')[0];
 			Dropdown.OptionData op = new Dropdown.OptionData(resolutionText);
 			dropdown.options.Add(op);
 		}
-		
 	}
+
+	public void ChangeResolution()
+	{
+		string res = label.text;
+		for(int i = 0; i < Screen.resolutions.Length; i++)
+		{
+			Resolution temp = Screen.resolutions[i];
+			if(temp.ToString().Split('@')[0].Equals(res))
+			{
+				Screen.SetResolution(temp.width, temp.height, toggle.GetComponent<Toggle>().isOn, temp.refreshRate);
+			}
+		}
+	}
+
 	public void ChangeScreenMode() 
 	{
 		if(toggle.GetComponent<Toggle>().isOn)
@@ -26,9 +41,5 @@ public class ResolutionSwitcher : MonoBehaviour {
 		{
 			Screen.fullScreen = false;
 		}
-	}
-	public void ChangeRes()
-	{
-		
 	}
 }
