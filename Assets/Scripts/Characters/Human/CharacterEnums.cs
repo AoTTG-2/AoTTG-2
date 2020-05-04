abcdc6d86f7ddbc3407ad15cb1c3e833e14fbee2
AlexWolski <alex.theinfinite@gmail.com>
2020-05-03 19:37:43 -0700

    Combined Hero enum files into one

---
+++ Assets/Scripts/Characters/Human/CharacterEnums.cs
@@ -0,0 +1,78 @@
+﻿public enum BODY_PARTS
+{
+    UPPER,
+    ARM_L,
+    ARM_R,
+    LOWER
+}
+
+public enum CreatePart
+{
+    Sex,
+    Eye,
+    Face,
+    Glass,
+    Hair,
+    Skin,
+    Costume,
+    Cape,
+    Division,
+    Preset
+}
+
+public enum SEX
+{
+    MALE,
+    FEMALE
+}
+
+public enum HERO_STATE
+{
+    Idle,
+    Attack,
+    GroundDodge,
+    AirDodge,
+    ChangeBlade,
+    FillGas,
+    ChangeGun,
+    Die,
+    Grab,
+    TitanForm,
+    Salute,
+    Slide,
+    Run,
+    Jump,
+    Land
+}
+
+public enum DIVISION
+{
+    TraineesSquad,
+    TheSurveryCorps,
+    TheGarrison,
+    TheMilitaryPolice
+}
+
+public enum UNIFORM_TYPE
+{
+    UniformA,
+    UniformB,
+    CasualA,
+    CasualB,
+    CasualAHSS
+}
+
+public enum CreateStat
+{
+    SPD,
+    GAS,
+    BLA,
+    ACL,
+    Skill
+}
+
+public enum BUFF
+{
+    NoBuff,
+    SpeedUp
+}
