de17659941964a751f793c52b74d2a62f8bf49db
ThatProBro2535 <agloria2001@gmail.com>
2020-05-15 20:06:18 -0600

    Added a fps counter and finished fps limiter

--- Assets/Scripts/UI/InGame/Graphics/FPSController.cs
+++ Assets/Scripts/UI/InGame/Graphics/FPSController.cs
@@ -6,8 +6,11 @@ using Newtonsoft.Json;
 
 public class FPSController : MonoBehaviour {
 
+	private float time;
 	[SerializeField] public Slider fpsLimiter;
 	[SerializeField] public Dropdown fpsDropdown;
+	[SerializeField] public Text fpsCounter;
+
 	public Dropdown FPSDropdown
 	{
 		get { return fpsDropdown; }
@@ -18,36 +21,68 @@ public class FPSController : MonoBehaviour {
 		get { return fpsLimiter; }
 		set { fpsLimiter = value; }
 	}
+	public Text FPSCounter
+	{
+		get { return fpsCounter; }
+		set { fpsCounter = value; }
+	}
+
+	private void Start() {
+		FPSDropdown.value = 5;
+		SetFPSLimit();
+		time = 0f;
+	}
+
+	private void Update() {
+		Counter();
+	}
+
 	public void SetFPSLimit()
 	{
-		if(fpsDropdown.itemText.text == "No Limit")
+		if(FPSDropdown.value == 6)
 		{
-			fpsLimiter.interactable = true;
+			FPSLimiter.interactable = true;
 			Application.targetFrameRate = (int)fpsLimiter.value;
 		}
 		else
 		{
-			fpsLimiter.interactable = false;
-			if(fpsDropdown.value == 0)
+			FPSLimiter.interactable = false;
+			if(FPSDropdown.value == 0)
 			{
 				Application.targetFrameRate = 30;
 			}
-			else if(fpsDropdown.value == 1)
+			else if(FPSDropdown.value == 1)
 			{
 				Application.targetFrameRate = 60;
 			}
-			else if(fpsDropdown.value == 2)
+			else if(FPSDropdown.value == 2)
 			{
 				Application.targetFrameRate = 120;
 			}
-			else if(fpsDropdown.value == 3)
+			else if(FPSDropdown.value == 3)
 			{
 				Application.targetFrameRate = 144;
 			}
-			else if(fpsDropdown.value == 4)
+			else if(FPSDropdown.value == 4)
 			{
 					Application.targetFrameRate = 240;
 			}
+			else
+			{
+				Application.targetFrameRate = -1;
+			}
 		}	
+	}
+
+	public void Counter()
+	{
+		time += Time.deltaTime;
+		if(time >= 1.0f)
+		{
+			var fps = 1.0f/Time.deltaTime;
+			FPSCounter.text = Convert.ToInt64(fps).ToString();
+			time = 0f;
+		}
+		
 	}
 }
