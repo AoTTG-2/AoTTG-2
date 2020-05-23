370fccd31f338274b0c60415b3a758825a1a141b
DjFrytek <65286359+DjFrytek@users.noreply.github.com>
2020-05-23 08:06:04 +0200

    #154 credits page (#155)
    * Created scrollable list on credits page
    
    -Added background image on credits page.
    -Created a list that can be scrolled with mouse button and scroll.
    -List scrolls on it's own after 2 seconds of not scrolling manually, stops when scrolled manually.
    
    * Added importing contributors list from a txt file
    
    Imports the list of contributors list from contributors.txt located in Assets folder and puts it in credits scrollable list.

---
+++ Assets/Scripts/UI/Menu/ImportingContributorsList.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImportingContributorsList : MonoBehaviour 
+{
+	public Text ContributorsList;
+	public TextAsset ContributorsTxtFile;
+	void Start () 
+	{
+		ContributorsList.text = ContributorsTxtFile.text;
+	}
+	
+
+}
---
+++ Assets/Scripts/UI/Menu/ScrollingScrollView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingScrollView : MonoBehaviour 
+{
+    public float ScrollSpeed = 0.5f;
+	private float timer = 0;
+	private float waitFor = 2f;
+    public RectTransform RectTransform;
+	
+	void OnEnable()
+	{
+		timer = 0;
+	}
+	
+	void Update()
+    {
+		if (Input.GetMouseButton(0) || Input.GetAxis("Mouse ScrollWheel") != 0)
+		{
+			timer = 0;
+		}
+		if(timer < waitFor) 
+		{
+			timer += Time.deltaTime;
+		}
+		else
+		{
+			RectTransform.localPosition += new Vector3(0f, ScrollSpeed, 0f);
+		}
+    }
+	
+}
