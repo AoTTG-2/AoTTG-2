7db0df0c4a1231485a8c5d377110305e0823ade0
DjFrytek <djfrytek123@gmail.com>
2020-05-23 06:18:57 +0200

    Added importing contributors list from a txt file
    Imports the list of contributors list from contributors.txt located in Assets folder and puts it in credits scrollable list.

---
+++ Assets/Scripts/UI/Menu/ImportingContributorsList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImportingContributorsList : MonoBehaviour {
+	public Text contributorsList;
+	public TextAsset contributorsTxtFile;
+	void Start () {
+		contributorsList.text = contributorsTxtFile.text;
+	}
+	
+
+}
