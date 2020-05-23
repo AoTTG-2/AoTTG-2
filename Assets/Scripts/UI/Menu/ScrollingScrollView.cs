ae572085aa6407d8e471a524ea15e792c00543e0
DjFrytek <djfrytek123@gmail.com>
2020-05-23 05:56:09 +0200

    Created scrollable list on credits page
    -Added background image on credits page.
    -Created a list that can be scrolled with mouse button and scroll.
    -List scrolls on it's own after 2 seconds of not scrolling manually, stops when scrolled manually.

---
+++ Assets/Scripts/UI/Menu/ScrollingScrollView.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollingScrollView : MonoBehaviour {
+
+    public float scrollSpeed = 0.5f;
+	private bool autoScroll = true;
+	private float timer = 0;
+	private float time = 0;	
+	private float waitFor = 2f;
+	public ScrollRect scrollRect;
+
+    public RectTransform rectTransform;
+
+	
+	void OnEnable()
+	{
+		scrollRect.verticalNormalizedPosition = 1;
+		timer = 0;
+	}
+	
+	void Update()
+    {
+		if (Input.GetMouseButton(0) || Input.GetAxis("Mouse ScrollWheel") != 0)
+		{
+			timer = 0;
+		}
+		if(timer < waitFor) 
+		{
+			timer += Time.deltaTime;
+		} else if(autoScroll) 
+		{
+			rectTransform.localPosition += new Vector3(0f, scrollSpeed, 0f);
+		}
+    }
+	
+}
