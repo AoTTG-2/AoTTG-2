ffd50f8c3e0a5759c0d991c0f7372e5b8595c975
JupiterJaeden <48777952+JupiterJaeden@users.noreply.github.com>
2020-05-26 00:05:24 -0400

    First attempt at CursorManagement class
    It doesn't work properly, but I'll try and fix it later.

---
+++ Assets/Scripts/UI/CursorManagement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CursorManagement : MonoBehaviour 
+{
+    private bool savedVisible;
+    private CursorLockMode savedLockState;
+
+    public static void switchToMenuMode()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None; 
+    }
+    public static void switchToLoadingOrTPSMode()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public static void switchToOriginalOrWOWMode()
+    {
+        Cursor.visible = false; 
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus)
+        {
+            //Load saved Cursor state
+            Cursor.visible = this.savedVisible;
+            Cursor.lockState = this.savedLockState; 
+        }
+        else
+        {
+            //Save current Cursor state
+            this.savedVisible = Cursor.visible;
+            this.savedLockState = Cursor.lockState;
+        }
+    }
+
+}
