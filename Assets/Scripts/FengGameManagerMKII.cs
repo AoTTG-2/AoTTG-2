e6d7aba6e7076ab9867a6788740fe01f834fc6d0
MrLurkin <61768971+MrLurkin@users.noreply.github.com>
2020-06-06 12:44:37 -0400

    Quick Ver Change

--- Assets/Scripts/FengGameManagerMKII.cs
+++ Assets/Scripts/FengGameManagerMKII.cs
@@ -16,7 +16,7 @@ using UnityEngine;
 //[Obsolete]
 public class FengGameManagerMKII : Photon.MonoBehaviour
 {
-    public static string Version = "Alpha-Issue36";
+    public static string Version = "Alpha-Issue186";
     public static bool showHackMenu = true;
 
     public Dictionary<int, CannonValues> allowedToCannon;
