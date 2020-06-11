9d2dd2c336e13c2ebc2ff8c03742b5b3d3283703
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2020-06-11 14:48:57 +0200

    Add VersionManager
    VersionManager sets the version to the branch name automatically.

---
+++ Assets/Scripts/VersionManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+[CreateAssetMenu, ExecuteInEditMode]
+public sealed class VersionManager : ScriptableObject
+{
+    [SerializeField]
+    private string version = string.Empty;
+
+    public string Version => version;
+
+#if UNITY_EDITOR
+
+    private void OnEnable() => TryGetBranchName(ref version);
+
+    private bool TryGetBranchName(ref string branchName)
+    {
+        try
+        {
+            var startInfo = new ProcessStartInfo("git.exe")
+            {
+                UseShellExecute = false,
+                WorkingDirectory = Directory.GetCurrentDirectory(),
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                Arguments = "rev-parse --abbrev-ref HEAD"
+            };
+
+            using (var process = new Process
+            {
+                StartInfo = startInfo
+            })
+            {
+                process.Start();
+                branchName = process.StandardOutput.ReadLine();
+                return true;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+
+        return false;
+    }
+
+#endif
+}
