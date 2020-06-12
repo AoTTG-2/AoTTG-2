6f15b174965f0dc33a8e8865a487d02b67657cab
ThatProBro2535 <agloria2001@gmail.com>
2020-06-11 18:01:33 -0600

    Added display switch

--- Assets/Scripts/UI/InGame/Graphics/ResolutionSwitcher.cs
+++ Assets/Scripts/UI/InGame/Graphics/ResolutionSwitcher.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using NUnit.Framework.Constraints;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +10,8 @@ namespace Assets.Scripts.UI.InGame
 	public class ResolutionSwitcher : MonoBehaviour {
 
 		public Dropdown Dropdown;
+		public Dropdown ARDropdown;
+		public Dropdown MDropdown;
 		public Toggle Toggle;
 		public Text DropDownLabel;
 
@@ -16,18 +20,42 @@ namespace Assets.Scripts.UI.InGame
 
 		private void OnEnable() {
 			Toggle.isOn = Screen.fullScreen;
+			string[] aspectRatios = new string[Screen.resolutions.Length];
+			var temp = "";
+			bool check = false;
+
+			for(int i = 0; i < Display.displays.Length; i++)
+			{
+				Dropdown.OptionData op = new Dropdown.OptionData(i.ToString());
+			}
 
 			foreach (Resolution resolution in Screen.resolutions)
 			{
-				string resolutionText = resolution.ToString();
-				Dropdown.OptionData op = new Dropdown.OptionData(resolutionText);
-				Dropdown.options.Add(op);
-				if (resolution.width == Screen.width && resolution.height == Screen.height)
+				var resolutionInfo = resolution.ToString().Split('@');
+				
+				if (temp != resolutionInfo[1] &&  temp != "")
 				{
-					Dropdown.value = Dropdown.options.IndexOf(op);
+					Debug.Log(resolution);
+					check = true;
+					Dropdown.OptionData _op = new Dropdown.OptionData(resolutionInfo[1]);
+					ARDropdown.options.Add(_op);
 				}
-
+				if(!check)
+				{
+					Dropdown.OptionData op = new Dropdown.OptionData(resolutionInfo[0]);
+					Dropdown.options.Add(op);
+					if (resolution.width == Screen.width && resolution.height == Screen.height)
+					{
+						Dropdown.value = Dropdown.options.IndexOf(op);
+					}
+				}
+				temp = resolutionInfo[1];
 			}
+		}
+
+		public void ChangeMonitor()
+		{
+			Display.displays[MDropdown.value].Activate();
 		}
 
 		public void ChangeResolution()
