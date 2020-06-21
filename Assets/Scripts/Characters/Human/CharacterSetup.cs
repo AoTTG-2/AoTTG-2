a088454c31b1714d805dfe5417ab6b9da0084510
AlexWolski <alex.theinfinite@gmail.com>
2020-06-20 23:49:15 -0700

    Moved character prefab references into a scriptable object

--- Assets/Scripts/Characters/Human/CharacterSetup.cs
+++ Assets/Scripts/Characters/Human/CharacterSetup.cs
@@ -1,13 +1,6 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CharacterSetup : MonoBehaviour
 {
-    [SerializeField] public List<GameObject> maleHair;
-    [SerializeField] public List<GameObject> femaleHair;
-    [SerializeField] public List<GameObject> maleUniforms;
-    [SerializeField] public List<GameObject> femaleUniforms;
-    [SerializeField] public List<GameObject> skin;
-    [SerializeField] public List<GameObject> equipment;
-    [SerializeField] public GameObject emblems;
+    [SerializeField] private CharacterPrefabs characterPrefabs;
 }
