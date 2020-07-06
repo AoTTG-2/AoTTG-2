b933a34a84fa392f7448ed5700bc4e5a8641c332
marzio giaquinta <you@example.com>
2020-07-06 23:06:22 +0200

    Fixxed Female Titan Label

--- Assets/Scripts/Characters/Titan/FEMALE_TITAN.cs
+++ Assets/Scripts/Characters/Titan/FEMALE_TITAN.cs
@@ -1102,33 +1102,36 @@ public class FEMALE_TITAN : MonoBehaviour
                 this.healthLabel = (GameObject) UnityEngine.Object.Instantiate(Resources.Load("UI/LabelNameOverHead"));
                 this.healthLabel.name = "LabelNameOverHead";
                 this.healthLabel.transform.parent = base.transform;
-                this.healthLabel.transform.localPosition = new Vector3(0f, 52f, 0f);
-                float a = 4f;
-                if ((this.size > 0f) && (this.size < 1f))
-                {
-                    a = 4f / this.size;
-                    a = Mathf.Min(a, 15f);
-                }
-                this.healthLabel.transform.localScale = new Vector3(a, a, a);
-            }
-            string str = "[7FFF00]";
-            float num2 = ((float) health) / ((float) maxHealth);
-            if ((num2 < 0.75f) && (num2 >= 0.5f))
-            {
-                str = "[f2b50f]";
-            }
-            else if ((num2 < 0.5f) && (num2 >= 0.25f))
-            {
-                str = "[ff8100]";
-            }
-            else if (num2 < 0.25f)
-            {
-                str = "[ff3333]";
-            }
-            //this.healthLabel.GetComponent<UILabel>().text = str + Convert.ToString(health);
-        }
-    }
+                this.healthLabel.transform.localPosition = new Vector3(0f, 50f + (5f / size), 5f);
 
+                float x = 1f;
+                if (size < 1f)
+                {
+                    x = 1f / size;
+                }
+
+                x *= 0.2f;
+                healthLabel.transform.localScale = new Vector3(x, x, x);
+
+
+            }
+        }
+        string color = "7FFF00";
+        float num2 = ((float) health) / ((float) maxHealth);
+        if ((num2 < 0.75f) && (num2 >= 0.5f))
+        {
+            color = "f2b50f";
+        }
+        else if ((num2 < 0.5f) && (num2 >= 0.25f))
+        {
+            color = "ff8100";
+        }
+        else if (num2 < 0.25f)
+        {
+            color = "ff3333";
+        }
+        healthLabel.GetComponent<TextMesh>().text = $"<color=#{color}>{health}</color>";
+    }
     public void lateUpdate()
     {
         if (!IN_GAME_MAIN_CAMERA.isPausing || (IN_GAME_MAIN_CAMERA.gametype != GAMETYPE.SINGLE))
@@ -1198,7 +1201,7 @@ public class FEMALE_TITAN : MonoBehaviour
         }
         bool mipmap = true;
         bool iteratorVariable1 = false;
-        if (((int)FengGameManagerMKII.settings[0x3f]) == 1)
+        if (((int) FengGameManagerMKII.settings[0x3f]) == 1)
         {
             mipmap = false;
         }
@@ -1215,16 +1218,16 @@ public class FEMALE_TITAN : MonoBehaviour
                     iteratorVariable1 = true;
                     iteratorVariable4.material.mainTexture = iteratorVariable6;
                     FengGameManagerMKII.linkHash[2].Add(url, iteratorVariable4.material);
-                    iteratorVariable4.material = (Material)FengGameManagerMKII.linkHash[2][url];
+                    iteratorVariable4.material = (Material) FengGameManagerMKII.linkHash[2][url];
                 }
                 else
                 {
-                    iteratorVariable4.material = (Material)FengGameManagerMKII.linkHash[2][url];
+                    iteratorVariable4.material = (Material) FengGameManagerMKII.linkHash[2][url];
                 }
             }
             else
             {
-                iteratorVariable4.material = (Material)FengGameManagerMKII.linkHash[2][url];
+                iteratorVariable4.material = (Material) FengGameManagerMKII.linkHash[2][url];
             }
         }
         if (iteratorVariable1)
@@ -1863,6 +1866,7 @@ public class FEMALE_TITAN : MonoBehaviour
     {
         if ((this.healthLabel != null)) //&& this.healthLabel.GetComponent<UILabel>().isVisible)
         {
+
             this.healthLabel.transform.LookAt(((Vector3) (2f * this.healthLabel.transform.position)) - Camera.main.transform.position);
         }
     }
