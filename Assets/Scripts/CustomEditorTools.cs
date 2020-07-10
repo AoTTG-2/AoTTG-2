c03c8d36c861cc0f7883fcf4e89113454b830a38
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2020-07-11 01:38:55 +0200

    fix: broken serialized fields in Hero.cs

---
+++ Assets/Scripts/CustomEditorTools.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CustomEditorTools : MonoBehaviour
+{
+    [MenuItem("Assets/Tools/Set Dirty")]
+    private static void SetDirty()
+    {
+        foreach (var o in Selection.objects)
+            EditorUtility.SetDirty(o);
+    }
+}
