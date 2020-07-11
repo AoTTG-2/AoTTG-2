10d2c75ca31d93528084bfa1083ffb9f67d70bce
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2020-07-11 02:30:12 +0200

    feat: add wall cannon
    The model does not have textures yet.

--- Assets/Scripts/Usable/Cannon/CannonTrajectory.cs
+++ Assets/Scripts/Usable/Cannon/CannonTrajectory.cs
@@ -16,14 +16,9 @@ namespace Cannon
         private void Reset()
         {
             lineRenderer = GetComponent<LineRenderer>();
-            lineRenderer.positionCount = 100;
+            lineRenderer.positionCount = 25;
             newPositions = new Vector3[lineRenderer.positionCount];
             lineRenderer.SetPositions(newPositions);
-            lineRenderer.widthMultiplier = 1.5f;
-            lineRenderer.startWidth = 0.5f;
-            lineRenderer.endWidth = 40f;
-            lineRenderer.startColor = lineRenderer.endColor = new Color(0f, 1f, 0f, 0.588f);
-            lineRenderer.widthCurve = AnimationCurve.EaseInOut(0f, 1f / 3f, 1f, 1f);
         }
 
         private void Start()
