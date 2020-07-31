4759c7ca7bae2011937f7e48971d682e6be35eb5
Lithrun <nickwindt@hotmail.nl>
2020-07-31 09:51:51 +0200

    Marked GAMETYPE.Single as obsolete

--- Assets/Scripts/Gamemode/Legacy/GAMETYPE.cs
+++ Assets/Scripts/Gamemode/Legacy/GAMETYPE.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [AddComponentMenu("Camera-Control/Mouse Look")]
 public enum GAMETYPE
 {
+    [Obsolete("Use PhotonNetwork.OfflineMode instead as per #184 Singleplayer", true)]
     SINGLE,
     SERVER,
     CLIENT,
