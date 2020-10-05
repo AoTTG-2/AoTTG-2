af62784467a1de9ddaae8f6210fbb5e9df14e23e
TheBoredProgrammer64 <63388183+TheBoredProgrammer64@users.noreply.github.com>
2020-10-05 21:46:45 +0300

    Update TimeSwitcher.cs

--- Assets/DayNightCycle/Scripts/TimeSwitcher.cs
+++ Assets/DayNightCycle/Scripts/TimeSwitcher.cs
@@ -22,12 +22,9 @@ namespace Assets.Scripts.UI.InGame
         public void UpdateTime()
         {
             int sValue = (int)Slider.value;
-<<<<<<< HEAD
+<
             DayAndNightControl.timeMultiplier = sValue;
-=======
-           // DayAndNightControl.timeMultiplier = sValue;
 
->>>>>>> 9727c57904a93c2692494582a681a2a6cf0a16f9
         }
 
         public struct TimeData
