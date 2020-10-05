75397d79a3686c01bc489cfb905574d98f22e5b7
TheBoredProgrammer64 <63388183+TheBoredProgrammer64@users.noreply.github.com>
2020-10-05 20:35:50 +0300

    Added a field to TimeSwitcher.cs

--- Assets/DayNightCycle/Scripts/TimeSwitcher.cs
+++ Assets/DayNightCycle/Scripts/TimeSwitcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,14 +8,21 @@ namespace Assets.Scripts.UI.InGame
 {
     public class TimeSwitcher : MonoBehaviour
     {
+        public DayAndNightControl DayAndNightControl;
+
         public Text Label;
         public Slider Slider;
+
+        public void Awake()
+        {
+            var dayNightObject = GameObject.Find("DayNightSystem");
+            DayAndNightControl = dayNightObject.GetComponent<DayAndNightControl>();
+        }
 
         public void UpdateTime()
         {
             int sValue = (int)Slider.value;
             DayAndNightControl.timeMultiplier = sValue;
-
         }
 
         public struct TimeData
