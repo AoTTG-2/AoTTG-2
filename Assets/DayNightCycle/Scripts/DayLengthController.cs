36891789f44c694fcdccf8cac048fa289fcfd37b
FransStreicher <fransie99@gmail.com>
2020-10-28 19:52:08 +0200

    Added Day length input field

--- Assets/DayNightCycle/Scripts/DayLengthController.cs
+++ Assets/DayNightCycle/Scripts/DayLengthController.cs
@@ -5,42 +5,42 @@ using UnityEngine.UI;
 
 namespace Assets.Scripts.UI.InGame
 {
-    public class TimeSwitcher : MonoBehaviour
+    public class DayLengthController : MonoBehaviour
     {
 
         public Text Label;
-        public Slider TimeSlider;
-        public InputField TimeInput;
-        private string time ;
-        private double seconds ;
 
-        
+        public InputField DayLengthInput;
+        private string time;
+        private double dayLength;
+
+
         DayAndNightControl DayNightCycle;
-       void Start()
+        void Start()
         {
             DayNightCycle = GameObject.Find("Day and Night Controller(Clone)").GetComponent<DayAndNightControl>();
             var input = gameObject.GetComponent<InputField>();
             var se = new InputField.SubmitEvent();
             se.AddListener(SubmitTime);
             input.onEndEdit = se;
-            TimeSlider.value = DayNightCycle.currentTime;
+            
         }
 
         void Update()
         {
-            
+
             Debug.Log(DayNightCycle.SecondsInAFullDay);
-            DayNightCycle.currentTime = TimeSlider.value;
+           // DayNightCycle.currentTime = TimeSlider.value;
         }
 
         private void SubmitTime(string arg0)
         {
-            Debug.Log(arg0);
-            time = arg0;
-            seconds = System.TimeSpan.Parse(time).TotalSeconds;
-            TimeSlider.value= (float) (seconds / 86400);
-            DayNightCycle.currentTime = (float) (seconds/86400);
-            Debug.Log((float) (seconds / 86400));
+           // Debug.Log(arg0);
+            
+            dayLength=float.Parse(arg0);
+            
+            DayNightCycle.SecondsInAfullDay = (float) dayLength;
+            Debug.Log(dayLength);
         }
 
         //here put functions that u wanna call from the change button at ServerSettingsPage.cs, I left
