c2f4e42ac8ccbde5bfeae3365641e9f33d42e4ba
8veFable <29248979+8veStory@users.noreply.github.com>
2021-01-11 21:39:35 +1100

    Fix sonarcloud issues

--- Assets/Scripts/Characters/Titan/MindlessTitan.cs
+++ Assets/Scripts/Characters/Titan/MindlessTitan.cs
@@ -7,7 +7,6 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Assets.Scripts.Gamemode.Options;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
