f84dc729597401ce3a165bbdd66ebac450722445
Toorah <tugratoorah@gmail.com>
2021-01-15 01:35:03 +0100

    Created HeroAudio class
    Splitting Hero Class further  into chunks

---
+++ Assets/Scripts/Audio/HeroAudio.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Audio.Hero
+{
+    [System.Serializable]
+    public class HeroAudio
+    {
+        public AudioSource audioDie;
+        public AudioSource rope;
+        public AudioSource slash;
+        public AudioSource slashHit;
+    }
+}
