d0f76cb82bacf6d7b24eea7875dca636d6712d82
Toorah <tugratoorah@gmail.com>
2021-01-15 01:34:38 +0100

    Created HookUI Script
    Splitting up the Hero Class into smaller chunks has begun

---
+++ Assets/Scripts/UI/InGame/HUD/HookUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.InGame.HUD
+{
+    public class HookUI
+    {
+        public Transform cross;
+        public Transform crossL;
+        public Transform crossR;
+
+        public Image crossImage;
+        public Image crossImageL;
+        public Image crossImageR;
+
+        public Text distanceLabel;
+
+        public bool enabled = false;
+
+        public void Find()
+        {
+            cross = GameObject.Find("cross1").transform;
+            crossImage = cross.GetComponentInChildren<Image>();
+            crossL = GameObject.Find("crossL1").transform;
+            crossImageL = crossL.GetComponentInChildren<Image>();
+            crossR = GameObject.Find("crossR1").transform;
+            crossImageR = crossR.GetComponentInChildren<Image>();
+
+            distanceLabel = GameObject.Find("Distance").GetComponent<Text>();
+
+            Enable();
+        }
+
+        public void Disable()
+        {
+            if (enabled)
+            {
+                cross.gameObject.SetActive(false);
+                crossImage.gameObject.SetActive(false);
+                crossL.gameObject.SetActive(false);
+                crossImageL.gameObject.SetActive(false);
+                crossR.gameObject.SetActive(false);
+                crossImageR.gameObject.SetActive(false);
+
+                distanceLabel.gameObject.SetActive(false);
+
+                enabled = false;
+            }
+        }
+        public void Enable()
+        {
+            if (!enabled)
+            {
+                cross.gameObject.SetActive(true);
+                crossImage.gameObject.SetActive(true);
+                crossL.gameObject.SetActive(true);
+                crossImageL.gameObject.SetActive(true);
+                crossR.gameObject.SetActive(true);
+                crossImageR.gameObject.SetActive(true);
+
+                distanceLabel.gameObject.SetActive(true);
+
+                enabled = true;
+            }
+        }
+    }
+
+}
