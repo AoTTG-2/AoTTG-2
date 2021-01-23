2d2893d9037a3d0e13fceb0fcdc510208a54befe
Rekperion <60749783+Rekperion@users.noreply.github.com>
2021-01-23 17:58:29 -0330

    Update FPSLimiter.cs

--- Assets/Scripts/UI/InGame/Graphics/FPSLimiter.cs
+++ Assets/Scripts/UI/InGame/Graphics/FPSLimiter.cs
@@ -24,19 +24,22 @@ public class FPSLimiter : MonoBehaviour
         }
     }
 
+    /*function to change the FPS limit*/
     public void SetFPSLimit()
     {
         FPSLimit.interactable = true;
         var text = FPSLimit.text;
-        int i;
-        Int32.TryParse(text.ToString(), out i);
-        Application.targetFrameRate = i;
+        int limit;
+        Int32.TryParse(text.ToString(), out limit);
+        Application.targetFrameRate = limit;
+        /*if the field is empty it will set it back to the default. This prevents the FPS going nuts upon clearing the limit*/
         if (FPSLimit.text == "")
         {
             DefaultFPS();
         }
     }
 
+    /*function for setting the FPS to the refresh rate*/
     public void DefaultFPS()
     {
         Application.targetFrameRate = Screen.currentResolution.refreshRate;
