16767534857eb4d40d24a1bc6f0556b6381e7fac
8veFable <29248979+8veStory@users.noreply.github.com>
2021-01-26 20:30:41 +1100

    Fix PauseRpc.

--- Assets/Scripts/Services/Interface/IPauseService.cs
+++ Assets/Scripts/Services/Interface/IPauseService.cs
@@ -8,8 +8,8 @@ namespace Assets.Scripts.Services.Interface
         float PauseTimer { get; }
 
         bool IsPaused();
-        void Pause(bool value, bool immediate = false);
-        void PauseRpc(PhotonMessageInfo info);
+        void Pause(bool shouldPause, bool immediate = true);
+        void PauseRpc(bool shouldPause, bool immediate, PhotonMessageInfo info);
 
         event EventHandler OnPaused;
         event EventHandler OnUnPaused;
