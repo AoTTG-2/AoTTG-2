531aa89280d16846d1a29756c3b11932014b6f36
DaSquyd <david.jacquish@septyr.com>
2021-01-31 00:50:51 -0500

    Fixed minimap spazzing

--- Assets/MinimapIcon.cs
+++ Assets/MinimapIcon.cs
@@ -5,21 +5,28 @@ namespace AOT.UI
     public class MinimapIcon : MonoBehaviour
     {
         private Rigidbody rb;
+        private Vector3 lastVelocity;
 
         private void Start()
         {
             rb = GetComponentInParent<Rigidbody>();
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
             if (transform != null)
                 transform.position = new Vector3(transform.parent.position.x, 245f, transform.parent.position.z);
 
-            if (rb != null && rb.velocity != Vector3.zero)
+            if (rb != null)
             {
-                var y = Quaternion.LookRotation(rb.velocity).eulerAngles.y;
-                transform.rotation = Quaternion.Euler(new Vector3(90f, y, 0f));
+                if (rb.velocity.magnitude <= 1f)
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(90f, transform.parent.rotation.eulerAngles.y, 0f));
+                }
+                else
+                {
+                    transform.rotation = Quaternion.Euler(new Vector3(90f, Quaternion.LookRotation(rb.velocity).eulerAngles.y, 0f));
+                }
             }
         }
     }
