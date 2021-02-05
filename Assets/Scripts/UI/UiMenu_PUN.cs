fba724564bb2e39972d3a21e56ff8610b70709b3
8veFable <29248979+8veStory@users.noreply.github.com>
2021-02-05 20:19:05 +1100

    Add UiMenu_PUN for UiNavigationElement

--- Assets/Scripts/UI/UiMenu_PUN.cs
+++ Assets/Scripts/UI/UiMenu_PUN.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace Assets.Scripts.UI
 {
-    class UiMenu_PUN : PunBehaviour, IUiContainer
+    public class UiMenu_PUN : PunBehaviour, IUiContainer
     {
         public virtual bool IsVisible() => gameObject.activeSelf;
 
