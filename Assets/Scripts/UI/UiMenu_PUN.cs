a9fab1c26fc496d3adf6823a4d5e7ca0950b7b02
8veFable <29248979+8veStory@users.noreply.github.com>
2021-02-05 19:40:25 +1100

    Get rid of unecessary usings

--- Assets/Scripts/UI/UiMenu_PUN.cs
+++ Assets/Scripts/UI/UiMenu_PUN.cs
@@ -1,10 +1,6 @@
 ﻿using Assets.Scripts.UI.Menu;
 using Photon;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assets.Scripts.UI
 {
