0fb9ffdb5bbacf483f77239d5fc0a086ddb4d6f1
Wattana Gaming <artjang301@gmail.com>
2021-02-05 11:54:32 +0700

    Dirty fix to keep CI build from failing
    Why tf is our CI including editor scripts anyway?

--- Assets/Editor/PrefabReplacer.cs
+++ Assets/Editor/PrefabReplacer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -38,5 +39,5 @@ namespace Assets.Editor.Prefabs
             }
         }
     }
-
 }
+#endif
