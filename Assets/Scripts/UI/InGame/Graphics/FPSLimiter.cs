77297107df76a8c24f8767148bd36241518cde9f
Rekperion <60749783+Rekperion@users.noreply.github.com>
2021-02-12 18:58:36 -0330

    Update FPSLimiter.cs

--- Assets/Scripts/UI/InGame/Graphics/FPSLimiter.cs
+++ Assets/Scripts/UI/InGame/Graphics/FPSLimiter.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class FPSLimiter : MonoBehaviour
 {
     public InputField fpsLimiter;
-    public int limit; //public so it can be easily modified and accessed by other classes
+    public int limit; ///public so it can be easily modified and accessed by other classes
 
     public InputField FPSLimit
     {
@@ -15,7 +15,6 @@ public class FPSLimiter : MonoBehaviour
 
     private void Start()
     {
-        /* Will first check to see if a limit has been set in the past and use that */
         if (FPSLimit.text == "")
         {
             DefaultFPS();
@@ -26,25 +25,22 @@ public class FPSLimiter : MonoBehaviour
         }
     }
 
-    /* function to change the FPS limit */
     public void SetFPSLimit()
     {
         FPSLimit.interactable = true;
         var text = FPSLimit.text;
-        text = text.Replace("-", string.Empty); //prevents negative values from doing funky stuff
+        text = text.Replace("-", string.Empty); ///prevents negative values from breaking fps limit
         Int32.TryParse(text.ToString(), out limit);
         Application.targetFrameRate = limit;
-        /* if the field is empty it will set it back to the default. This prevents the FPS going nuts upon clearing the limit */
         if (FPSLimit.text == "")
         {
             DefaultFPS();
         }
     }
 
-    /* function for setting the FPS to the refresh rate */
     public void DefaultFPS()
     {
-        Application.targetFrameRate = Screen.currentResolution.refreshRate; //VSync
+        Application.targetFrameRate = Screen.currentResolution.refreshRate; ///VSync
     }
 
     public struct FPSData
@@ -56,5 +52,4 @@ public class FPSLimiter : MonoBehaviour
             this.field = toCopy.FPSLimit.text;
         }
     }
-
 }
