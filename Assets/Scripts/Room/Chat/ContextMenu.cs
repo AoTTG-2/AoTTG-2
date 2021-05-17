50b880cac3622bd391d71e40c61abf84446f7cf2
Adonis <adonis.carvajal2203@gmail.com>
2021-05-17 00:04:46 -0400

    Moved the AddListener to Start because they dont belong in Update

--- Assets/Scripts/Room/Chat/ContextMenu.cs
+++ Assets/Scripts/Room/Chat/ContextMenu.cs
@@ -1,6 +1,7 @@
 using UnityEngine.UI;
 using Assets.Scripts.Room.Chat;
 
+
 public class ContextMenu : Photon.MonoBehaviour
 {
 
@@ -10,22 +11,21 @@ public class ContextMenu : Photon.MonoBehaviour
     private TextTranslator translator = new TextTranslator();
 
 
-    void Update()
+    void Start()
     {
 
-        if (gameObject.activeSelf)
-        {
-
-            Copy.onClick.AddListener(CopyMessage);
-            Translate.onClick.AddListener(TranslateMessage);
-
-        }
+        Copy.onClick.AddListener(CopyMessage);
+        Translate.onClick.AddListener(TranslateMessage);
 
     }
 
     private void TranslateMessage()
     {
         //Translate stuff and turn the context menu off
+
+        //TODO figure out how to import the SystemLanguageConverter.GetSystemLanguageCultureCode() function from UnityEngine.Localization package
+
+        //StartCoroutine(translator.Translate(clickedOver, "auto", SystemLanguageConverter.GetSystemLanguageCultureCode(Application.systemLanguage));
     }
 
     void CopyMessage()
