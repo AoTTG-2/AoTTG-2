81dfdd960791937d77a0465948a8278a14f83bd4
Velsh-arc <burakpolatyavan@hotmail.com>
2021-06-10 23:24:05 +0300

    New tree models are added and tweaked

---
+++ Assets/Resources/TheForestRevampedResources/Animation/PlayAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+     [SerializeField] private Animator animationController;
+
+    public GameObject Tree;
+    public GameObject Particles;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Enemy has collided with tree");
+            Particles.SetActive(true);
+            animationController.SetBool("playFall", true);
+            Invoke("DestroyTree", 4);
+
+        }
+    }
+    public void DestroyTree(){
+        Destroy(Tree);
+    }
+}
+
+
