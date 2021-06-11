336ce12f3ac643474b6d54f56072d67e8fa44794
Velsh-arc <burakpolatyavan@hotmail.com>
2021-06-11 15:42:51 +0300

    Script is optimised, unnecessary models are removed

--- Assets/Resources/TheForestRevampedResources/Animation/PlayAnimation.cs
+++ Assets/Resources/TheForestRevampedResources/Animation/PlayAnimation.cs
@@ -10,25 +10,34 @@ public class PlayAnimation : MonoBehaviour
     public GameObject Particles;
     public float distance = 15f;
     GameObject[] titans;
+    bool treeHit = false;
+    bool particleplay = false;
     
-
-    public void Update()
+    public void Start(){
+        InvokeRepeating("Hit", 0.0f, 0.5f);
+        
+    }
+    
+    public void Hit()
     {
-        titans = GameObject.FindGameObjectsWithTag("titan"); //This is a horrible way to solve this issue, especially since this has to be inside the Update(),
-        //Extremely perfromance heavy and needs a better way
+        titans = GameObject.FindGameObjectsWithTag("titan"); //This is a horrible way to solve this issue, it's a performance hog
+        Vector3 position = transform.position;
         foreach (GameObject titan in titans){
             float enemdistance = Vector3.Distance(titan.transform.position, transform.position);
-            if (enemdistance <= distance){
-                Debug.Log("Titan has collided with tree");
+            if (enemdistance <= distance && treeHit==false){
+                GameObject.Instantiate(Particles, position, Quaternion.identity);
+                //Debug.Log("Titan has collided with tree");
                 Particles.SetActive(true);
                 animationController.SetBool("playFall", true); 
                 Invoke("DestroyTree", 4);
+                treeHit=true;
             }
         }   
     }
     public void DestroyTree(){
         Destroy(Tree);
     }
+    
 }
 
 
