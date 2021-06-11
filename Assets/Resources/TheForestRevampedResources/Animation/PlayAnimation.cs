e933f848907ebc19066da027d892a037cc2b5dd3
Velsh-arc <burakpolatyavan@hotmail.com>
2021-06-11 13:16:04 +0300

    Tree script is fixed, better audio added

--- Assets/Resources/TheForestRevampedResources/Animation/PlayAnimation.cs
+++ Assets/Resources/TheForestRevampedResources/Animation/PlayAnimation.cs
@@ -9,17 +9,22 @@ public class PlayAnimation : MonoBehaviour
     public GameObject Tree;
     public GameObject Particles;
     public float distance = 15f;
+    GameObject[] titans;
     
 
     public void Update()
     {
-        if (Vector3.Distance(GameObject.FindWithTag("titan").transform.position, transform.position) <= distance)
-        {
-            Debug.Log("Titan has collided with tree");
-            Particles.SetActive(true);
-            animationController.SetBool("playFall", true); 
-            Invoke("DestroyTree", 4);
-        }
+        titans = GameObject.FindGameObjectsWithTag("titan"); //This is a horrible way to solve this issue, especially since this has to be inside the Update(),
+        //Extremely perfromance heavy and needs a better way
+        foreach (GameObject titan in titans){
+            float enemdistance = Vector3.Distance(titan.transform.position, transform.position);
+            if (enemdistance <= distance){
+                Debug.Log("Titan has collided with tree");
+                Particles.SetActive(true);
+                animationController.SetBool("playFall", true); 
+                Invoke("DestroyTree", 4);
+            }
+        }   
     }
     public void DestroyTree(){
         Destroy(Tree);
