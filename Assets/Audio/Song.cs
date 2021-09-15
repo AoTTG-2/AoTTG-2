4e819692a4082a1c1209581e013970125fc1b694
DjFrytek <djfrytek123@gmail.com>
2021-09-15 23:17:11 +0200

    Created Song scriptable object

---
+++ Assets/Audio/Song.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Song", menuName = "Song")]
+public class Song : ScriptableObject
+{
+    public string songName;
+    public AudioClip clip;
+
+    public string type = "neutral";
+
+    [Range(0f, 1f)]
+    public float volume = 1f;
+}
