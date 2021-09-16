f6e417bd9b19014b351e99aaf8788fd98cfce096
DjFrytek <djfrytek123@gmail.com>
2021-09-16 02:53:12 +0200

    Yet another update of AudioController

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -30,13 +30,26 @@ public class AudioController : MonoBehaviour
 
     private void Start()
     {
-        PlayMusic();
+        PlaySong("neutral", 0);
+        PlaySong("combat", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (HasSongEnded(neutralAudioSource))
+        {
+            PlayNextSong("neutral");
+        }
+        if (HasSongEnded(combatAudioSource))
+        {
+            PlayNextSong("combat");
+        }
+    }
 
+    public void PlayNextSong(string type)
+    {
+        PlaySong(type, (type == "neutral" ? neutralPlaylistIndex : combatPlaylistIndex) + 1);
     }
 
     public void SwitchMusic(AudioMixerSnapshot snapshot)
@@ -45,11 +58,66 @@ public class AudioController : MonoBehaviour
         currentSnapshot.TransitionTo(transitionTime);
     }
 
+    private bool HasSongEnded(AudioSource audioS)
+    {
+        return (!audioS.isPlaying);
+    }
+
     public void NeutralCombatSwap()
     {
         SwitchMusic(currentSnapshot == neutralSnapshot ? combatSnapshot : neutralSnapshot);
     }
 
+    public void LoadPlaylist(string type, Playlist playlist)
+    {
+        if (type == "neutral")
+        {
+            neutralPlaylist = playlist;
+            neutralPlaylistIndex = 0;
+            combatPlaylistIndex = 0;
+        }
+        else if (type == "combat")
+        {
+            combatPlaylist = playlist;
+            neutralPlaylistIndex = 0;
+            combatPlaylistIndex = 0;
+        }
+        PlaySong("neutral", 0);
+        PlaySong("combat", 0);
+    }
+
+    public void PlaySong(string type, int index) {
+        if (type == "neutral")
+        {
+            neutralPlaylistIndex = index;
+            neutralPlaylistIndex = neutralPlaylistIndex % neutralPlaylist.songs.Length;
+        } else if (type == "combat")
+        {
+            combatPlaylistIndex = index;
+            combatPlaylistIndex = combatPlaylistIndex % combatPlaylist.songs.Length;
+        }
+
+        LoadSongSettings(type);
+        (type == "neutral" ? neutralAudioSource : combatAudioSource).Play();
+    }
+
+    private void LoadSongSettings(string type)
+    {
+        Song currentSong;
+        switch (type)
+        {
+            case "neutral":
+                currentSong = neutralPlaylist.songs[neutralPlaylistIndex];
+                neutralAudioSource.volume = currentSong.volume;
+                neutralAudioSource.clip = currentSong.clip;
+                break;
+            case "combat":
+                currentSong = combatPlaylist.songs[combatPlaylistIndex];
+                combatAudioSource.volume = currentSong.volume;
+                combatAudioSource.clip = currentSong.clip;
+                break;
+        }
+    }
     private void PlayMusic()
     {
         Song currentNeutralSong = neutralPlaylist.songs[neutralPlaylistIndex];
