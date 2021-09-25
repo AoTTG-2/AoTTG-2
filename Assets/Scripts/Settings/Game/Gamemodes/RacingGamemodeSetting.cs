3655c2f4eb87e6c04835fd80e51fd6f19fc0104e
Lithrun <nickwindt@hotmail.nl>
2021-09-25 16:57:44 +0200

    Add Akina

---
+++ Assets/Scripts/Settings/Game/Gamemodes/RacingGamemodeSetting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Settings.Game.Gamemodes
+{
+    class RacingGamemodeSetting
+    {
+    }
+}
