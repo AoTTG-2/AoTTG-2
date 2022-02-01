165e6f97fd23a22f7e7e189b1c32546742f72d15
Retrovending <80718352+Retrovending@users.noreply.github.com>
2022-02-01 12:13:48 -0600

    Added MLAgents Package
    -MLAgents Package
    -AI Agent Script

---
+++ Assets/Scripts/AI Soldier/SoldierAI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.mlagents;
+using Unity.mlagents.actuators;
+
+public class SoldierAI : Agent
+{
+    override public void OnCollisionActivation(OnAction actions) { 
+    }
+}
