a54e58414cf87901bf7eda65e75daf398196eea0
Tobias Wedelin <tobias.wedelin@live.se>
2022-02-24 18:34:36 +0100

    refactoring

--- Assets/Scripts/Audio/AudioController.cs
+++ Assets/Scripts/Audio/AudioController.cs
@@ -5,24 +5,24 @@ using UnityEngine.Audio;
 namespace Assets.Scripts.Audio
 {
     /// <summary>
-    /// Includes common parameters and logic that is to be shared between controller dealing with audio. Inherits <see cref="SingeltonMonoBehaviour{Tclass}"/>
+    /// Includes common parameters and logic that is to be shared between controllers dealing with audio. Inherits <see cref="SingeltonMonoBehaviour{Tclass}"/>
     /// </summary>
     /// <typeparam name="Tclass"></typeparam>
     public abstract class AudioController<Tclass> : SingeltonMonoBehaviour<Tclass> where Tclass : class
     {
+        #region Private Properties
+        private const float MinVolume = 0.0001f;
+        private const float MaxVolume = 1;
+        #endregion
+
         #region Public Properties
         [Tooltip("Audio mixer group that the controller has responsibility for")]
         public AudioMixerGroup MixerGroup;
         [Tooltip("Current volume of the attatched audio mixer group")]
-        [Range(0.0001f, 1f)]
+        [Range(MinVolume, MaxVolume)]
         public float Volume;
         [Tooltip("The name of the attatched audio mixer group's exposed volume parameter")]
         public string VolumeParameterName;
-        #endregion
-
-        #region Private Properties
-        private float minVolume = 0.0001f;
-        private float maxVolume = 1;
         #endregion
 
         #region Protected Properties
@@ -46,7 +46,7 @@ namespace Assets.Scripts.Audio
         #region Protected Methods
         protected float GetLogVolume(float volume)
         {
-            return volume == 0 ? minVolume : volume > 1 ? maxVolume : volume;
+            return volume == 0 ? MinVolume : volume > 1 ? MaxVolume : volume;
         }
         #endregion
     }
